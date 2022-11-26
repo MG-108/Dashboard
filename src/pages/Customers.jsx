cd9563a1fe760ef7304ea1fcf11a96f75cc184c8
Matheus Ghignatti <matheusghignatti@gmail.com>
2022-11-26 16:34:42 -0300

    add Customers

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid, contextMenuItems } from "../data/dummy";
+import { Header } from "../components";
 
 const Customer = () => {
-  return <div>Customer</div>;
+  const editing = { allowDeleting: true, allowEditing: true };
+
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={["Delete"]}
+        width="auto"
+        editSettings={editing}
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Toolbar, Edit, Selection, Sort, Filter]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Customer;
